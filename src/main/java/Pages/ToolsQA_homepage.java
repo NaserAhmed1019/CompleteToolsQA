f70739d89e714ffa5d9133ba208561cf97953771
Naser Ahmed <naserahmed1019@gmail.com>
2023-02-09 14:59:51 -0500

    first commit

---
+++ src/main/java/Pages/ToolsQA_homepage.java
@@ -0,0 +1,22 @@
+package Pages;
+
+import org.openqa.selenium.By;
+
+public class ToolsQA_homepage {
+
+    public String url = "https://demoqa.com/widgets";
+
+// Elements
+    public By elementLocator = new By.ByCssSelector(".element-group:first-child [d='M0 0h24v24H0z']");
+    public By textBoxLocator = new By.ByCssSelector("");
+    public By checkBoxLocator = new By.ByCssSelector("");
+    public By radioButtonLocator = new By.ByCssSelector("");
+    public By webTablesLocator = new By.ByCssSelector("");
+    public By buttonsLocator = new By.ByCssSelector("");
+    public By linksLocator = new By.ByCssSelector("");
+    public By brokenLinks_imagesLocator = new By.ByCssSelector("");
+    public By uploadAndDownloadsLocator = new By.ByCssSelector("");
+    public By dynamicPropertiesLocator = new By.ByCssSelector("");
+
+
+}
